339f48fe12d2b96f1f8a233a547626a3ea4234a5
Josiah <jojotenia@gmail.com>
2023-04-20 22:57:51 -0400

    init with bug fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SquareComponent } from './square/square.component';
+import { BoardComponent } from './board/board.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, SquareComponent, BoardComponent],
+  imports: [BrowserModule, AppRoutingModule ],
   providers: [],
   bootstrap: [AppComponent]
 })
